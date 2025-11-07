a40cf35179046d072429b360df3d306ecd3095d1
DmitryAstapenko <dmitryastapenko1@gmail.com>
2025-11-07 21:09:44 +0300

    feat: complete carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -17,11 +17,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== "string") return false;
+
+  const activity = Number.parseFloat(sampleActivity);
+  const result = Math.ceil(
+    Math.log(MODERN_ACTIVITY / activity) / (0.693 / HALF_LIFE_PERIOD)
+  );
+
+  if (result && activity > 0 && activity <= MODERN_ACTIVITY) {
+    return result;
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
-  dateSample
+  dateSample,
 };
