ae8b90b0ee8cef93ed3cf67f099a339c9f2f66b0
DmitryAstapenko <dmitryastapenko1@gmail.com>
2025-11-09 14:18:53 +0300

    feat: complete dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,34 @@ const { NotImplementedError } = require('../lib');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function getDNSStats(domains) {
+  const domainMap = new Map();
+  const allDomains = domains
+    .map((domain) =>
+      domain
+        .split(".")
+        .reverse()
+        .map((_, index, array) => {
+          let resultDomain = "";
+          for (let i = 0; i < index + 1; i++) {
+            resultDomain = resultDomain.concat(".", `${array[i]}`);
+          }
+          return resultDomain;
+        })
+    )
+    .flat();
+
+  allDomains.forEach((domain) => {
+    if (domainMap.has(domain)) {
+      domainMap.set(domain, domainMap.get(domain) + 1);
+    } else {
+      domainMap.set(domain, 1);
+    }
+  });
+
+  return Object.fromEntries(domainMap);
 }
 
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
