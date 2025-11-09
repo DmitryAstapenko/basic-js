783e77b8660461dccb9c0baa35acb293c67babb5
DmitryAstapenko <dmitryastapenko1@gmail.com>
2025-11-09 17:21:13 +0300

    feat: complete minesweeper

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,36 @@ const { NotImplementedError } = require('../lib');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+  const sweeper = [];
+
+  const check = (item) => {
+    return item ? 1 : 0;
+  };
+
+  for (let i = 0; i < matrix.length; i += 1) {
+    sweeper.push([]);
+
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j]) {
+        sweeper[i][j] = 1;
+      } else {
+        sweeper[i][j] =
+          check(matrix[i - 1]?.[j - 1]) +
+          check(matrix[i - 1]?.[j]) +
+          check(matrix[i - 1]?.[j + 1]) +
+          check(matrix[i]?.[j - 1]) +
+          check(matrix[i]?.[j + 1]) +
+          check(matrix[i + 1]?.[j - 1]) +
+          check(matrix[i + 1]?.[j]) +
+          check(matrix[i + 1]?.[j + 1]);
+      }
+    }
+  }
+
+  return sweeper;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
